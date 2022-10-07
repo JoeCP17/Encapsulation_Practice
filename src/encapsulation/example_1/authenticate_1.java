159ee89f322251bd09f1366009236c183d00d2a2
Ueibin <ats1113@zuminternet.com>
2022-10-08 02:31:04 +0900

    수정

--- src/encapsulation/example_1/authenticate_1.java
+++ src/encapsulation/example_1/authenticate_1.java
@@ -2,7 +2,6 @@ package example_1;// 캡슐화 연습 1
 
 
 public class authenticate_1 {
-    /*
     //변환 전
     Member mem = findOne(id);
         if(mem == null) return AuthResult.NO_MATCH;
@@ -13,17 +12,16 @@ public class authenticate_1 {
 
         return AuthResult.NO_MATCH;
 
-     */
 
     // 변환 후
-//    Member mem = findOne(id);
-//
-//    if(mem == null) return AuthResult.NO_MATCH;
-//
-//    if(!mem.isEmailVerified()) return AuthResult.NO_EMAIL_VERIFIED; // 검증 부분을 객체가 하도록 설정 최대한 객체에게 일을 시켜 비지니스로직의
-//    // 변경을 최소화한다.
-//
-//    if(passwordEncoder.isPasswordValid(mem.getPassword(), pw, mem.getId())) return AuthResult.SUCCESS;
-//
-//    return AuthResult.NO_MATCH;
+    Member mem = findOne(id);
+
+    if(mem == null) return AuthResult.NO_MATCH;
+
+    if(!mem.isEmailVerified()) return AuthResult.NO_EMAIL_VERIFIED; // 검증 부분을 객체가 하도록 설정 최대한 객체에게 일을 시켜 비지니스로직의
+    // 변경을 최소화한다.
+
+    if(passwordEncoder.isPasswordValid(mem.getPassword(), pw, mem.getId())) return AuthResult.SUCCESS;
+
+    return AuthResult.NO_MATCH;
 }
